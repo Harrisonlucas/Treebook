cae2d917c0a29c1ec425fc13fff5a3f137be85b9
Harry Lucas <harrison.lucas@live.com>
2014-05-14 16:49:52 +1000

    User authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :statuses
-
-  # The priority is based upon order of creation: first created -> highest priority.
+  root to: 'statuses#index'  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
